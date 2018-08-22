687458dc0df04f867d73843b5e5ad04b8b7d7c26
nickboldt <nboldt@redhat.com>
2018-08-22 13:34:14 -0400

    new script to run a build in NCL, including node/npm settings, proxy config for node/npm/bower, http workaround for phantomjs 2.1.1, and console timestamping
    Change-Id: Ia45817878c0185ea4f1e292c3350c89f06177cf9
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    fail build if bash fails (-e); remove bower stuff as it doesn't help; reduce log verbosity
    
    Change-Id: I0cc64b5f50ce57e7a844b4c52f2df0be4bb59844
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    add 10881-replace-bower-with-yarn.patch to build script
    
    Change-Id: I0e9ee111bb1f9caa970153a39695c855d4a94b24
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    set path to patch file relative to repo root
    
    Change-Id: I1f071beec5ca22c3d9fcc14ec6123b979dd43a02
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    update script: remove bower stuff, add time tracking on wget and npm operations, switch to maxsockets 80 and -T 16
    
    Change-Id: I6ebc9a1aad64a0cbf40b5cf850ce5377c583289c
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    fix patch command
    
    Change-Id: I28a0851d5819ba03c6e41e590b802442d98a4200
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ product/build-ncl.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -xe
+# script to build eclipse-che in #projectncl
+
+uname -a
+go version
+node -v
+npm version
+mvn -v
+
+export NCL_PROXY="http://${buildContentId}+tracking:${accessToken}@${proxyServer}:${proxyPort}"
+# wget proxies
+export http_proxy="${NCL_PROXY}"
+export https_proxy="${NCL_PROXY}"
+
+nodeDownloadRoot=http://nodejs.org:80/dist/
+npmDownloadRoot=http://registry.npmjs.org:80/npm/-/
+npmRegistryURL=http://registry.npmjs.org:80/
+npm config set https-proxy ${NCL_PROXY}
+npm config set proxy ${NCL_PROXY}
+#silent, warn, info, verbose, silly
+npm config set loglevel warn 
+npm config set maxsockets 80
+npm config set fetch-retries 10
+npm config set fetch-retry-mintimeout 60000
+npm config set registry ${npmRegistryURL}
+
+# workaround for lack of https support and inability to see github.com as a result
+mkdir -p /tmp/phantomjs/
+pushd /tmp/phantomjs/
+  # previously mirrored from https://github.com/Medium/phantomjs/releases/download/v2.1.1/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  time wget -q http://download.jboss.org/jbosstools/updates/requirements/node/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+popd
+pushd dashboard
+  time npm install phantomjs-prebuilt
+  export PATH=${PATH}:`pwd`/node_modules/phantomjs-prebuilt/bin
+  time npm install yarn
+  export PATH=${PATH}:`pwd`/node_modules/yarn/bin
+  export TMPDIR=/tmp
+  yarn config set proxy http://username:password@host:port
+  yarn config set https-proxy http://username:password@host:port
+
+  # apply patch to move from bower to yarn
+  # https://github.com/nickboldt/che/tree/10881
+  # path relative to root since we run this script as ./product/build-ncl.sh
+  patch -p 2 -F 2 < ../product/10881-replace-bower-with-yarn.patch
+popd
+
+mvn clean deploy -T 16 -V -ff -B -e '-Pfast,native,!docker' -Dskip-enforce -DskipTests -Dskip-validate-sources -Dfindbugs.skip -DskipIntegrationTests=true \
+-Dmdep.analyze.skip=true -Dmaven.javadoc.skip -Dgpg.skip -Dorg.slf4j.simpleLogger.showDateTime=true \
+-Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+-DnodeDownloadRoot=${nodeDownloadRoot} -DnpmDownloadRoot=${npmDownloadRoot} -DnpmRegistryURL=${npmRegistryURL}
+
+uname -a
+go version
+node -v
+npm version
+mvn -v
