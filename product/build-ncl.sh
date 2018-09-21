1cc6b06613a020b875bcb1d42f72431ab2a3839c
nickboldt <nboldt@redhat.com>
2018-09-21 12:04:14 -0400

    include dashboard from upstream binary; add better commandline syntax
    Change-Id: I149888796921125fca75d32389edd77977497847
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/build-ncl.sh
+++ product/build-ncl.sh
@@ -11,6 +11,9 @@
 # fix dashboard - migrate to Yarn
 # git cherry-pick --keep-redundant-commits 3005b907815118c8ebef75a09d51798e0b052077
 
+# don't build dashboard from source; include from upstream binary in http://oss.sonatype.org/content/repositories/snapshots/
+includeDashboardFromSource=0
+
 # remove docs from assembly-main - requires using '-P!docs' profile
 # git cherry-pick --keep-redundant-commits c1fa62ae86f976d97247e726458f6e25ccf0611f
 
@@ -20,30 +23,39 @@
 
 #set version & compute qualifier from best available in Indy
 # or use commandline overrides for version and suffix
+version=6.12.0
+suffix="" # normally we compute this from version of org/eclipse/che/depmgt/maven-depmgt-pom but can override if needed
 
-if [[ $1 ]]; then
-	version="$1"
-else
-	version=6.12.0
+# read commandline args
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+		'-v') version="$2"; shift 1;; #eg., 6.12.0
+		'-s') suffix="$2"; shift 1;; # eg., redhat-00007
+		'-dv') includeDashboardVersion="$2"; shift 1;; # eg., 6.11.1 or 6.12.0-SNAPSHOT
+		*) OTHER="${OTHER} $1"; shift 0;; 
+	esac
+	shift 1
+done
+
+if [[ ! ${suffix} ]]; then # compute it from version of org/eclipse/che/depmgt/maven-depmgt-pom
+  tmpfile=/tmp/maven-depmgt-pom-${version}.html
+  # external 1: http://indy.cloud.pnc.engineering.redhat.com/api/group/static/org/eclipse/che/depmgt/maven-depmgt-pom
+  # external 2: http://indy.cloud.pnc.engineering.redhat.com/api/content/maven/group/builds-untested+shared-imports+public/org/eclipse/che/depmgt/maven-depmgt-pom
+  UPSTREAM_POM="api/content/maven/group/builds-untested+shared-imports+public/org/eclipse/che/depmgt/maven-depmgt-pom"
+  INDY=http://indy.project-newcastle.svc.cluster.local
+  if [[ ! $(wget ${INDY} -q -S 2>&1 | egrep "200|302|OK") ]]; then
+    INDY=http://pnc-indy-branch-nightly.project-newcastle.svc.cluster.local
+  fi
+  if [[ ! $(wget ${INDY} -q -S 2>&1 | egrep "200|302|OK") ]]; then
+    echo "[WARNING] Could not load org/eclipse/che/depmgt/maven-depmgt-pom from Indy"
+  fi
+  wget ${INDY}/${UPSTREAM_POM} -O ${tmpfile}
+  suffix=$(grep ${version} ${tmpfile} | egrep '.redhat-[0-9]{5}' | sed -e "s#.\+>\([0-9.]\+\.\)\(redhat-[0-9]\{5\}\).*#\2#" | sort -r | head -1)
+  rm -f ${tmpfile}
 fi
 
-if [[ $2 ]]; then
-	suffix="$2"
-else
-	tmpfile=/tmp/maven-depmgt-pom-${version}.html
-	# external 1: http://indy.cloud.pnc.engineering.redhat.com/api/group/static/org/eclipse/che/depmgt/maven-depmgt-pom
-	# external 2: http://indy.cloud.pnc.engineering.redhat.com/api/content/maven/group/builds-untested+shared-imports+public/org/eclipse/che/depmgt/maven-depmgt-pom
-	UPSTREAM_POM="api/content/maven/group/builds-untested+shared-imports+public/org/eclipse/che/depmgt/maven-depmgt-pom"
-	INDY=http://indy.project-newcastle.svc.cluster.local
-	if [[ ! $(wget ${INDY} -q -S 2>&1 | egrep "200|302|OK") ]]; then
-		INDY=http://pnc-indy-branch-nightly.project-newcastle.svc.cluster.local
-	fi
-	if [[ ! $(wget ${INDY} -q -S 2>&1 | egrep "200|302|OK") ]]; then
-		echo "[WARNING] Could not load org/eclipse/che/depmgt/maven-depmgt-pom from Indy"
-	fi
-	wget ${INDY}/${UPSTREAM_POM} -O ${tmpfile}
-	suffix=$(grep ${version} ${tmpfile} | egrep '.redhat-[0-9]{5}' | sed -e "s#.\+>\([0-9.]\+\.\)\(redhat-[0-9]\{5\}\).*#\2#" | sort -r | head -1)
-	rm -f ${tmpfile}
+if [[ ! ${includeDashboardVersion} ]]; then
+  includeDashboardVersion=${version}-SNAPSHOT
 fi
 
 # replace pme version with the version from upstream parent pom, so we can resolve parent pom version 
@@ -56,12 +68,12 @@ fi
 # pmeVersionSHA=$(git describe --tags)
 # pmeSuffix=${pmeVersion#${version}.}; echo $suffix
 if [[ ${suffix} ]]; then 
-	for d in $(find . -name pom.xml); do sed -i "s#\(version>\)${version}.*\(</version>\)#\1${version}.${suffix}\2#g" $d; done
-	for d in $(find . -name pom.xml); do sed -i "s#\(<che.\+version>\)${version}.*\(</che.\+version>\)#\1${version}.${suffix}\2#g" $d; done
-	for d in $(find . -name pom.xml); do sed -i "s#\(<version>${version}\)-SNAPSHOT#\1.${suffix}#g" $d; done
-	mvn versions:set -DnewVersion=${version}.${suffix}
-	mvn versions:update-parent "-DparentVersion=${version}.${suffix}" -DallowSnapshots=false
-	for d in $(find . -maxdepth 1 -name pom.xml); do sed -i "s#\(<.\+\.version>.\+\)-SNAPSHOT#\1.${suffix}#g" $d; done
+  for d in $(find . -name pom.xml); do sed -i "s#\(version>\)${version}.*\(</version>\)#\1${version}.${suffix}\2#g" $d; done
+  for d in $(find . -name pom.xml); do sed -i "s#\(<che.\+version>\)${version}.*\(</che.\+version>\)#\1${version}.${suffix}\2#g" $d; done
+  for d in $(find . -name pom.xml); do sed -i "s#\(<version>${version}\)-SNAPSHOT#\1.${suffix}#g" $d; done
+  mvn versions:set -DnewVersion=${version}.${suffix}
+  mvn versions:update-parent "-DparentVersion=${version}.${suffix}" -DallowSnapshots=false
+  for d in $(find . -maxdepth 1 -name pom.xml); do sed -i "s#\(<.\+\.version>.\+\)-SNAPSHOT#\1.${suffix}#g" $d; done
 fi
 
 ##########################################################################################
@@ -96,40 +108,65 @@ npm config set fetch-retry-mintimeout 60000
 npm config set registry ${npmRegistryURL}
 npm config list
 
-# workaround for lack of https support and inability to see github.com as a result
-mkdir -p /tmp/phantomjs/
-pushd /tmp/phantomjs/
-	# previously mirrored from https://github.com/Medium/phantomjs/releases/download/v2.1.1/phantomjs-2.1.1-linux-x86_64.tar.bz2
-	time wget -q http://download.jboss.org/jbosstools/updates/requirements/node/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
-popd
-pushd dashboard
-	time npm install phantomjs-prebuilt
-	export PATH=${PATH}:`pwd`/node_modules/phantomjs-prebuilt/bin
 
-	time npm install yarn
-	PATH=${PATH}:`pwd`/node_modules/yarn/bin
-	yarn config set registry ${YARN_REGISTRY} --global
-	yarn config set proxy ${NCL_PROXY} --global
-	yarn config set yarn-proxy ${NCL_PROXY} --global
-	yarn config set yarn_proxy ${NCL_PROXY} --global
-	yarn config set https-proxy ${NCL_PROXY} --global
-	yarn config set https_proxy ${NCL_PROXY} --global
-	yarn config set YARN_REGISTRY ${YARN_REGISTRY} --global
-	yarn config list
-	yarn install --frozen-lockfile --no-lockfile --pure-lockfile --ignore-optional --non-interactive --production=false
-popd
+if [[ $includeDashboardFromSource -gt 0 ]]; then
+  # workaround for lack of https support and inability to see github.com as a result
+  mkdir -p /tmp/phantomjs/
+  pushd /tmp/phantomjs/
+    # previously mirrored from https://github.com/Medium/phantomjs/releases/download/v2.1.1/phantomjs-2.1.1-linux-x86_64.tar.bz2
+    time wget -q http://download.jboss.org/jbosstools/updates/requirements/node/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  popd
+
+  pushd dashboard
+    time npm install phantomjs-prebuilt
+    export PATH=${PATH}:`pwd`/node_modules/phantomjs-prebuilt/bin
+
+    time npm install yarn
+    PATH=${PATH}:`pwd`/node_modules/yarn/bin
+    yarn config set registry ${YARN_REGISTRY} --global
+    yarn config set YARN_REGISTRY ${YARN_REGISTRY} --global
+
+    yarn config set proxy ${NCL_PROXY} --global
+    yarn config set yarn-proxy ${NCL_PROXY} --global
+    yarn config set yarn_proxy ${NCL_PROXY} --global
+
+    yarn config set https-proxy false --global
+    yarn config set https_proxy false --global
+    yarn config list
+    yarn install --frozen-lockfile --no-lockfile --pure-lockfile --ignore-optional --non-interactive --production=false
+  popd
+fi
+
+# fetch latest dashboard or snapshot
+if [[ $includeDashboardVersion ]]; then
+  pushd /tmp/
+    mvn dependency:get -DgroupId=org.eclipse.che.dashboard -DartifactId=che-dashboard-war -Dversion=${includeDashboardVersion} -Dpackaging=war \
+     -DremoteRepositories=http://oss.sonatype.org/content/repositories/snapshots/
+    mvn dependency:get -DgroupId=org.eclipse.che.dashboard -DartifactId=che-dashboard-war -Dversion=${includeDashboardVersion} -Dclassifier=sources  \
+     -DremoteRepositories=http://oss.sonatype.org/content/repositories/snapshots/
+    dashboardm2Folder=${HOME}/.m2/repository/org/eclipse/che/dashboard/che-dashboard-war/${includeDashboardVersion}/
+    ls -la ${dashboardm2Folder}
+  popd
+  # get actualdashboard version, eg. 6.12.0-20180921.144339-40 instead of 6.12.0-SNAPSHOT
+  cheDashboardVersion=$(cat ${dashboardm2Folder}/maven-metadata-temp.xml | grep value | tail -1 | sed -e "s#.*<value>\(.\+\)</value>#\1#")
+fi
 
 ##########################################################################################
 # configure maven build 
 ##########################################################################################
 
 PROFILES='-Pfast,native,!docker,!docs'
+
 MVNFLAGS="-V -ff -B -e -Dskip-enforce -DskipTests -Dskip-validate-sources -Dfindbugs.skip -DskipIntegrationTests=true"
 MVNFLAGS="${MVNFLAGS} -Dmdep.analyze.skip=true -Dmaven.javadoc.skip -Dgpg.skip -Dorg.slf4j.simpleLogger.showDateTime=true"
 MVNFLAGS="${MVNFLAGS} -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss "
 MVNFLAGS="${MVNFLAGS} -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
 MVNFLAGS="${MVNFLAGS} -DnodeDownloadRoot=${nodeDownloadRoot} -DnpmDownloadRoot=${npmDownloadRoot}"
 MVNFLAGS="${MVNFLAGS} -DnpmRegistryURL=${npmRegistryURL} -DYARN_REGISTRY=${YARN_REGISTRY}"
+
+if [[ $includeDashboardVersion ]]; then
+  MVNFLAGS="${MVNFLAGS} -Dche.dashboard.version=${cheDashboardVersion}"
+fi
 
 ##########################################################################################
 # run maven build 
